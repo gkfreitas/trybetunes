d96c9116d99cef1b83776495b1a7af16c1458d8e
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-02-05 10:35:26 -0300

    REQ5 - Finalizado, input para procurar artista e botao para pesquisar criados

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,44 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    artistName: '',
+    buttonEnable: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, this.buttonEnableChange);
+  };
+
+  buttonEnableChange = () => {
+    const { artistName } = this.state;
+    if (artistName.length >= 2) this.setState({ buttonEnable: false });
+  };
+
   render() {
+    const { artistName, buttonEnable } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            name="artistName"
+            value={ artistName }
+            onChange={ this.handleChange }
+          />
+          <button
+            data-testid="search-artist-button"
+            disabled={ buttonEnable }
+          >
+            Pesquisar
+
+          </button>
+        </form>
       </div>
     );
   }
