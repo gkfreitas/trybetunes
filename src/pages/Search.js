7703fbd95fca85a8bc8f7cf6953624e5bf0bcbe4
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-02-05 14:46:19 -0300

    REQ6 - Finalizado, albuns renderizados

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from './Loading';
 
 class Search extends Component {
   state = {
     artistName: '',
     buttonEnable: true,
+    albumList: [],
+    renderAlbum: false,
+    loading: false,
   };
 
   handleChange = ({ target }) => {
@@ -19,8 +25,31 @@ class Search extends Component {
     if (artistName.length >= 2) this.setState({ buttonEnable: false });
   };
 
+  handleClick = async () => {
+    const { artistName } = this.state;
+    await searchAlbumsAPI(artistName);
+    this.setState({
+      loading: true,
+      artistName: '',
+      searchedArtist: artistName,
+    });
+    this.setState({
+      albumList: [await searchAlbumsAPI(artistName)],
+      renderAlbum: true,
+      loading: false,
+    });
+  };
+
   render() {
-    const { artistName, buttonEnable } = this.state;
+    const {
+      artistName,
+      albumList,
+      buttonEnable,
+      searchedArtist,
+      loading,
+      renderAlbum,
+    } = this.state;
+
     return (
       <div data-testid="page-search">
         <Header />
@@ -35,11 +64,35 @@ class Search extends Component {
           <button
             data-testid="search-artist-button"
             disabled={ buttonEnable }
+            onClick={ this.handleClick }
+            type="button"
           >
             Pesquisar
-
           </button>
         </form>
+        <h2>
+          Resultado de álbuns de:
+          {' '}
+          {searchedArtist}
+        </h2>
+        <ul>
+          {loading ? <Loading /> : ''}
+          {renderAlbum
+            ? albumList[0].map((e, i) => (
+              <li key={ `${e.artistName} ${e.trackCount} ${e.collectionPrice} ${i}` }>
+                <img src={ e.artworkUrl100 } alt={ e.artistName } />
+                <p>{e.artistName}</p>
+                <p>{e.collectionName}</p>
+                <Link
+                  data-testid={ `link-to-album-${e.collectionId}` }
+                  to={ `/album/${e.collectionId}` }
+                >
+                  Link
+                </Link>
+              </li>
+            ))
+            : 'Nenhum álbum foi encontrado'}
+        </ul>
       </div>
     );
   }
